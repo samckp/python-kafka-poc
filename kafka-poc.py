44c8ba13133ddc7ffb7f05184766ff4d7cea8a3d
samckp <sam.mca.liet@gmail.com>
2023-04-23 13:28:09 +0530

    updated

--- kafka-poc.py
+++ kafka-poc.py
@@ -3,11 +3,11 @@ from json import dumps
 from kafka import KafkaProducer
 
 topic_name='test-topic'
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda x:
+producer = KafkaProducer(bootstrap_servers=['172.29.192.1:9092'], value_serializer=lambda x:
 dumps(x).encode('utf-8'))
 
-for e in range(1000):
+for e in range(10):
     data = {'number' : e}
     print(data)
     producer.send(topic_name, value=data)
-    sleep(5)
+    sleep(0.005)
